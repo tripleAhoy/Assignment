b975375ddb30abf1bf01cb4ceffd25ce3c6e9b25
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-05 06:20:00 +0100

    Add PrimeNumbersFuntion.java file

---
+++ function/PrimeNumbersFuntion.java
@@ -0,0 +1,19 @@
+public class PrimeNumbersFuntion {
+
+public static boolean isPrime(int number) {
+		
+if(number <= 1) return false;
+		
+if(number == 2) return true;
+		
+if(number % 2 == 0) return false;
+		
+for(int i = 3; i * i <= number; i += 2) {			
+if(number % i == 0) return false;
+}
+
+return true;
+
+}
+
+}
