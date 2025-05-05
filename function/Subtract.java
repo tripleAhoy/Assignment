2dda50423ce756c98d151fb81ee841e35f4629e3
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-05 06:22:34 +0100

    Add Subtract.java file

---
+++ function/Subtract.java
@@ -0,0 +1,11 @@
+public class Subtract {
+
+	public static int subtract (int number1, int number2) {
+		if (number1 > number2) {
+			return number1 - number2;
+		}
+		else {
+			return number2 - number1;
+		}
+	}
+}
