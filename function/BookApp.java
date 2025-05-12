f6fd3b14cfc7ab6fe689f4749c1020daf37bf7d2
Adewale Adegbite <adegbiteolawale44@gmail.com>
2025-05-12 08:54:05 +0100

    Add BookApp.java file

--- function/BookApp.java
+++ function/BookApp.java
@@ -1,9 +1,40 @@
 public class BookApp {
 
 	public static int isNumberCopies(int copies) {
-		int price = 2000;
-		int priceCopies = price * copies; 
-
+		int price = 0;
+		int priceCopies = price * copies;
+		if(copies <= 4) {
+			price = 2000;
+			priceCopies = 0;
+		}
+		else if (copies >= 5 && copies <= 9) {
+			price = 1800;
+			priceCopies = price * copies;
+		}
+		else if (copies >= 10 && copies <= 29) {
+			price = 1500;
+			priceCopies = price * copies;
+		}
+		else if (copies >= 30 && copies <= 49) {
+			price = 1300;
+			priceCopies = price * copies;
+		}		 
+		else if (copies >= 50 && copies <= 99) {
+			price = 1300;
+			priceCopies = price * copies;
+		}
+		else if (copies >= 100 && copies <= 199) {
+			price = 1200;
+			priceCopies = price * copies;
+		}
+		else if (copies >= 200 && copies <= 499) {
+			price = 1100;
+			priceCopies = price * copies;
+		}
+		else if (copies >= 500) {
+			price = 1000;
+			priceCopies = price * copies;
+		}
 		return priceCopies;
 	}
 } 
